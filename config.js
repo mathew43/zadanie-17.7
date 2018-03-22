47456347287842d2d4ee6b6b94c2d13af09f9edd
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2018-03-22 17:17:33 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '623837213927-n74bbguk1bdachc9ekhmhp6vv3sfpind.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'Ad9peGn6vjAFAqETZfhTVT6n',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
